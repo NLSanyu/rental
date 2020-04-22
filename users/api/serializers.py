deeb0acf53c10a61869dd80d504dd1bdabf142a3
NLSanyu <lydiansanyu@gmail.com>
2020-04-22 10:30:56 +0300

    Set up user serializer

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -12,3 +12,22 @@ class RegistrationSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             'password': {'write_only': True}
         }
+
+    def save(self):
+        user = User(
+            email=self.validated_data['email'],
+            username = self.validated_data['username'],
+            first_name=self.validated_data['first_name'],
+            last_name=self.validated_data['last_name'],
+            phone_number=self.validated_data['phone_number'],
+        )
+
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
+
+        if password != password2:
+            raise serializers.ValidationError({'password': 'Passwords must match'})
+
+        user.set_password(password)
+        user.save()
+        return user
