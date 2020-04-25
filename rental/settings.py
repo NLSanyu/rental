636bd3d6c599ff4a59cec8b11b9032fa97e9ad53
NLSanyu <lydiansanyu@gmail.com>
2020-04-25 14:46:16 +0300

    Add requirements file

--- rental/settings.py
+++ rental/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'users',
-    'property',
+    'rental.apps.users',
+    'rental.apps.property',
 ]
 
 MIDDLEWARE = [
