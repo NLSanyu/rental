9d032a5031ce434d4483f297928a94e32836022b
NLSanyu <lydiansanyu@gmail.com>
2020-04-25 12:16:07 +0300

    Set up user manager class

--- rental/apps/users/models.py
+++ rental/apps/users/models.py
@@ -1,57 +1,42 @@
+import jwt
+
+from datetime import datetime, timedelta
+
+from django.conf import settings
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionsMixin
+)
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 class UserManager(BaseUserManager):
-	def create_user(self, email, username, password=None):
-		if not email:
-			raise ValueError('Users must have an email address')
-		if not username:
-			raise ValueError('Users must have a username')
+    """
+    Manager class required by Django for custom user classes
+    """
 
-		user = self.model(
-			email=self.normalize_email(email),
-			username=username,
-		)
+    def create_user(self, username, email, password=None):
+        """Create and return a User with an email, username and password"""
+        if username is None:
+            raise TypeError('Users must have a username.')
 
-		user.set_password(password)
-		user.save(using=self._db)
-		return user
+        if email is None:
+            raise TypeError('Users must have an email address.')
 
-	def create_superuser(self, email, username, password):
-		user = self.create_user(
-			email=self.normalize_email(email),
-			password=password,
-			username=username,
-		)
-		user.is_admin = True
-		user.is_staff = True
-		user.is_superuser = True
-		user.save(using=self._db)
-		return user
+        user = self.model(username=username, email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
 
-class User(AbstractBaseUser):
-    email = models.EmailField(verbose_name="email", max_length=60, unique=True)
-    username = models.CharField(max_length=30, unique=True)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    phone_number = models.CharField(max_length=30)
-    date_joined = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
-    last_login = models.DateTimeField(verbose_name='last login', auto_now=True)
-    is_admin = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
+        return user
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    def create_superuser(self, username, email, password):
+        """
+        Create and return a User with superuser (admin) permissions.
+        """
+        if password is None:
+            raise TypeError('Superusers must have a password.')
 
-    objects = UserManager()
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
 
-    def __str__(self):
-        return self.email
-
-    def has_perm(self, perm, obj=None):
-        return self.is_admin
-
-    def has_module_perms(self, app_label):
-        return True
+        return user
