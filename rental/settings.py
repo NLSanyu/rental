4e2fb2cfe0ed89601a94fa15e485ad8e00d43e64
NLSanyu <lydiansanyu@gmail.com>
2020-08-16 10:27:05 +0300

    Add email configurations

--- rental/settings.py
+++ rental/settings.py
@@ -83,6 +83,14 @@ REST_FRAMEWORK = {
 
 WSGI_APPLICATION = 'rental.wsgi.application'
 
+# Email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
